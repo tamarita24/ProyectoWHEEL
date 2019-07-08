6e9179c027031511752fbcafe7700372611e3026
tamy.valentina <you@example.comtamy.valentina@gmail.com>
2019-07-08 02:32:16 -0400

    mantenedor viaje

---
+++ ProyectoWheel/CapaGUI/MantenedorViajeCrear.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace CapaGUI
+{
+    partial class MantenedorViajeCrear
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblTarifaViaje = new System.Windows.Forms.Label();
+            this.lblLugarDestino = new System.Windows.Forms.Label();
+            this.lblNombreServicio = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LblComuna = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblTotalPago = new System.Windows.Forms.Label();
+            this.lblIdTipoPago = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblNombreConductor = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(42, 90);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(51, 13);
+            this.lblDistancia.TabIndex = 1;
+            this.lblDistancia.Text = "Distancia";
+            // 
+            // lblTarifaViaje
+            // 
+            this.lblTarifaViaje.AutoSize = true;
+            this.lblTarifaViaje.Location = new System.Drawing.Point(42, 120);
+            this.lblTarifaViaje.Name = "lblTarifaViaje";
+            this.lblTarifaViaje.Size = new System.Drawing.Size(60, 13);
+            this.lblTarifaViaje.TabIndex = 2;
+            this.lblTarifaViaje.Text = "Tarifa Viaje";
+            // 
+            // lblLugarDestino
+            // 
+            this.lblLugarDestino.AutoSize = true;
+            this.lblLugarDestino.Location = new System.Drawing.Point(42, 146);
+            this.lblLugarDestino.Name = "lblLugarDestino";
+            this.lblLugarDestino.Size = new System.Drawing.Size(88, 13);
+            this.lblLugarDestino.TabIndex = 3;
+            this.lblLugarDestino.Text = "Lugar de Destino";
+            // 
+            // lblNombreServicio
+            // 
+            this.lblNombreServicio.AutoSize = true;
+            this.lblNombreServicio.Location = new System.Drawing.Point(264, 52);
+            this.lblNombreServicio.Name = "lblNombreServicio";
+            this.lblNombreServicio.Size = new System.Drawing.Size(85, 13);
+            this.lblNombreServicio.TabIndex = 4;
+            this.lblNombreServicio.Text = "Nombre Servicio";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(79, 284);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(188, 284);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(298, 284);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLimpiar.TabIndex = 7;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(408, 284);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(362, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // LblComuna
+            // 
+            this.LblComuna.AutoSize = true;
+            this.LblComuna.Location = new System.Drawing.Point(42, 172);
+            this.LblComuna.Name = "LblComuna";
+            this.LblComuna.Size = new System.Drawing.Size(46, 13);
+            this.LblComuna.TabIndex = 10;
+            this.LblComuna.Text = "Comuna";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(109, 172);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(147, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // lblTotalPago
+            // 
+            this.lblTotalPago.AutoSize = true;
+            this.lblTotalPago.Location = new System.Drawing.Point(520, 88);
+            this.lblTotalPago.Name = "lblTotalPago";
+            this.lblTotalPago.Size = new System.Drawing.Size(59, 13);
+            this.lblTotalPago.TabIndex = 15;
+            this.lblTotalPago.Text = "Total Pago";
+            // 
+            // lblIdTipoPago
+            // 
+            this.lblIdTipoPago.AutoSize = true;
+            this.lblIdTipoPago.Location = new System.Drawing.Point(523, 66);
+            this.lblIdTipoPago.Name = "lblIdTipoPago";
+            this.lblIdTipoPago.Size = new System.Drawing.Size(56, 13);
+            this.lblIdTipoPago.TabIndex = 16;
+            this.lblIdTipoPago.Text = "Tipo Pago";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(584, 61);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(585, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // lblNombreConductor
+            // 
+            this.lblNombreConductor.AutoSize = true;
+            this.lblNombreConductor.Location = new System.Drawing.Point(45, 51);
+            this.lblNombreConductor.Name = "lblNombreConductor";
+            this.lblNombreConductor.Size = new System.Drawing.Size(96, 13);
+            this.lblNombreConductor.TabIndex = 19;
+            this.lblNombreConductor.Text = "Nombre Conductor";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(147, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 20;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(543, 284);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 21;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // MantenedorViajeCrear
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.lblNombreConductor);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.lblIdTipoPago);
+            this.Controls.Add(this.lblTotalPago);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.LblComuna);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblNombreServicio);
+            this.Controls.Add(this.lblLugarDestino);
+            this.Controls.Add(this.lblTarifaViaje);
+            this.Controls.Add(this.lblDistancia);
+            this.Name = "MantenedorViajeCrear";
+            this.Text = "Mantenedor Viaje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Label lblTarifaViaje;
+        private System.Windows.Forms.Label lblLugarDestino;
+        private System.Windows.Forms.Label lblNombreServicio;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label LblComuna;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblTotalPago;
+        private System.Windows.Forms.Label lblIdTipoPago;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblNombreConductor;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
